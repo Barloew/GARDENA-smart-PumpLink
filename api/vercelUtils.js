c0708a9e98f0d4d590a442d5cad320646798379c
Barloew <barry.loewer@gmail.com>
2024-10-29 11:49:12 +0100

    Create vercelUtils.js

---
+++ api/vercelUtils.js
@@ -0,0 +1,25 @@
+// api/vercelUtils.js
+
+const { getKVValue } = require('./kvHelpers');
+
+const getProductionUrl = (req) => {
+  const protocol = req.headers['x-forwarded-proto'] || 'https';
+  const host = req.headers.host || process.env.VERCEL_URL;
+  return `${protocol}://${host}`;
+};
+
+const getRedirectUrl = (req) => {
+  const productionUrl = getProductionUrl(req);
+  return `${productionUrl}/api/oauth?action=callback`;
+};
+
+const getWebhookUrl = (req) => {
+  const productionUrl = getProductionUrl(req);
+  return `${productionUrl}/api/webhook`; 
+};
+
+module.exports = {
+  getProductionUrl,
+  getRedirectUrl,
+  getWebhookUrl,
+};
